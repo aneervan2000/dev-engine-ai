f8e277fc76899f2ad5653ec29fcc14c364af8332
Aneervan Ray <aneervan.raja2000@gmail.com>
2025-12-15 17:42:28 +0530

    Refactor member management: update MemberResponse fields, change deleteProjectMember to removeProjectMember, and implement member invitation logic

--- src/main/java/com/anee/projects/lovable_clone/mapper/ProjectMemberMapper.java
+++ src/main/java/com/anee/projects/lovable_clone/mapper/ProjectMemberMapper.java
@@ -4,11 +4,17 @@ import com.anee.projects.lovable_clone.dto.member.MemberResponse;
 import com.anee.projects.lovable_clone.entities.ProjectMember;
 import com.anee.projects.lovable_clone.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProjectMemberMapper {
 
+    @Mapping(target = "userId", source = "id")
+    @Mapping(target = "projectRole", constant = "OWNER")
     MemberResponse toProjectMemberResponseFromOwner(User owner);
 
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "email", source = "user.email")
+    @Mapping(target = "name", source = "user.name")
     MemberResponse toProjectMemberResponseFromMember(ProjectMember projectMember);
 }
